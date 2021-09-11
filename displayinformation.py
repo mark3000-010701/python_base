990e1ffcbd3dbcc3be91ea06ac7adfff0154936e
mark3000-010701 <82459471+mark3000-010701@users.noreply.github.com>
2021-09-11 18:17:30 +0700

    Add files via upload

---
+++ displayinformation.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 10 20:07:53 2021
+
+@author: Tran Dinh Hoi
+"""
+import turtle
+import math
+# print("nhap toa do diem A:")
+# x = float(input('nhap hoanh do x: ' ))
+
+# y = float(input('nhap tung do y:'))
+
+
+# # chuvi = (a+b)*2
+# # print("chu ci la: ",(a + b ) * 2, end=' ')
+# # print("dien tich hcn là: ", a*b)
+
+#r = int(input("nhap ban kinh vô: "))
+#t = turtle.Turtle()
+#t.hideturtle()
+##t.color("red")
+#t.circle(r)
+#turtle.done()
+#c = 2 * math.pi * r
+#s = math.pi * r * r
+#print("Chu vi của hình tròn có bán kính = {r} là {c}".format(r=r, c=c))
+#print("Diện tích của hình tròn có bán kính = {r} là {s}".format(r=r, s=s))
+
+#Họ và tên, ngày sinh, địa chỉ, mục đích đến với khoá học Python.
+
+print('***************************************************')
+print("* Họ tên: Trần đình Hội                           *")
+print("* Ngày sinh: 01/07/2005                           *")
+print("* địa chỉ: Hà nội                                 *")
+print("* Mục đích: học để lấy kiến thwucs đi lm          *")
+print("***************************************************")
+
+
+
