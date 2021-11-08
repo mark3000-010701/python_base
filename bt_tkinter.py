ac39e75075ba29c906987489e74edf154850f315
mark3000-010701 <82459471+mark3000-010701@users.noreply.github.com>
2021-11-08 20:37:57 +0700

    Add files via upload

---
+++ bt_tkinter.py
@@ -0,0 +1,45 @@
+from tkinter import *
+#from tkinter import ttk
+windows=Tk()
+
+'''
+windows.title("weight conversion GUI")
+windows.geometry("500x300")
+
+lb_input=Label(windows,text="Enter the weight in Kg")
+entry_input=Entry(windows)
+def convert():
+    result=float(entry_input.get())*1000
+    lb_kq_gram.config(text=result)
+    result2=float(entry_input.get())*2.20462262
+    lb_kq_pounds.config(text=result2)
+    result3 = float(entry_input.get())*35.2739619
+    lb_kq_ounce.config(text=result3)
+btn_convert=Button(windows,text="Convert",command=convert)
+lb_gram=Label(windows,text="Gram")
+lb_pounds=Label(windows,text="Pounds")
+lb_ounce=Label(windows,text="Ounce")
+lb_kq_gram=Label(windows,text="")
+lb_kq_pounds=Label(windows,text="")
+lb_kq_ounce=Label(windows,text="")
+txt_gram=Text(windows)
+txt_pounds=Text(windows)
+txt_ounce=Text(windows)
+lb_input.grid(column=0,row=0)
+entry_input.grid(column=1,row=0)
+btn_convert.grid(column=2,row=0)
+lb_gram.grid(column=0,row=1)
+lb_pounds.grid(column=1,row=1)
+lb_ounce.grid(column=2,row=1)
+lb_kq_gram.grid(column=0,row=2)
+lb_kq_pounds.grid(column=1,row=2)
+lb_kq_ounce.grid(column=2,row=2)
+'''
+
+
+
+windows.mainloop()
+
+
+
+
